407cc34653dba29b17c7c4b37a776003d2d3afe4
rhiggins <rhiggins@angular-demo.myguest.virtualbox.org>
2025-05-09 12:48:59 -0400

    branding depending on screen size

--- src/app/core/components/main-toolbar/main-toolbar.component.ts
+++ src/app/core/components/main-toolbar/main-toolbar.component.ts
@@ -1,13 +1,18 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { SearchComponent } from './search/search.component';
 import { UserInfoComponent } from './user-info/user-info.component';
 
 @Component({
   imports: [
+    CommonModule,
     MatAutocompleteModule,
     MatToolbarModule,
     MatIconModule,
@@ -17,7 +22,19 @@ import { UserInfoComponent } from './user-info/user-info.component';
   ],
   selector: 'app-main-toolbar',
   standalone: true,
-  styleUrl: './main-toolbar.component.scss',
+  styleUrls: ['./main-toolbar.component.scss'],
   templateUrl: './main-toolbar.component.html',
 })
-export class MainToolbarComponent {}
+export class MainToolbarComponent {
+  iconSource$: Observable<string>;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.iconSource$ = this.breakpointObserver
+      .observe([Breakpoints.HandsetPortrait, Breakpoints.HandsetLandscape])
+      .pipe(
+        map((result) =>
+          result.matches ? 'img/brand_logo.png' : 'img/brand_name_logo.png',
+        ),
+      );
+  }
+}
