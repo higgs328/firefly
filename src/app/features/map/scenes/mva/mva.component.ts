2f058ba1f4b94131214f22ef8bb8ce0e61cf2f80
Higgins <robert.d.higgins@gmail.com>
2025-05-16 15:49:45 -0400

    added mapbox accessToken to config

--- src/app/features/map/scenes/mva/mva.component.ts
+++ src/app/features/map/scenes/mva/mva.component.ts
@@ -1,13 +1,13 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MapComponent, provideMapboxGL } from 'ngx-mapbox-gl';
+import { mapboxConfig } from '../../../../core/configs/mapbox.config';
 import { TrailLayerComponent } from '../../layers/trail/trail.component';
 
 @Component({
   imports: [MapComponent, TrailLayerComponent],
   providers: [
     provideMapboxGL({
-      accessToken:
-        'pk.eyJ1IjoicmhpZ2dpbnMtY3ZmZCIsImEiOiJja2kxdjlzZmMxZTVpMnhvZHNmNXZsdDFpIn0.ta6054evoS8aIkY4hpSLeQ',
+      accessToken: mapboxConfig.accessToken,
     }),
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
