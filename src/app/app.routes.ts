564be3d8a258effd55d9a9f60d98e4eeb1125ea2
Higgins <robert.d.higgins@gmail.com>
2025-05-16 11:43:10 -0400

    added mapbox provider to map component to reduce chunk / bundle size

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,11 +14,6 @@ export const routes: Routes = [
     children: [
       {
         loadComponent: () =>
-          import('./features/map/map.component').then((m) => m.MapComponent),
-        path: 'scene',
-      },
-      {
-        loadComponent: () =>
           import('./features/map/structure/map-structure.component').then(
             (m) => m.MapStructureComponent,
           ),
